7be400bb887ec51e4364990888182c0a6aaa351e
Harith Kavish S <94520772+HarithKavish@users.noreply.github.com>
2024-02-07 01:58:33 +0530

    Add files via upload

---
+++ Day2/src/components/Title.jsx
@@ -0,0 +1,11 @@
+const Title = () => {
+    return (
+        <>
+            <div id="title">
+                <>AgroCloud Finance Pro</>
+            </div>
+        </>
+    )
+}
+
+export default Title;
