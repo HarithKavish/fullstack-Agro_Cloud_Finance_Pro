10c0d2ba8b0681fa721c00c0cf110b0c59a28d7a
Harith Kavish S <94520772+HarithKavish@users.noreply.github.com>
2024-02-07 02:00:19 +0530

    Add files via upload

---
+++ Day4/src/components/TextField.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+
+const TextField = ({ type, name }) => {
+    return (
+        <input id='input' type={type} name={name} />
+    );
+};
+
+TextField.propTypes = {
+    type: PropTypes.node.isRequired,
+    name: PropTypes.string.isRequired
+};
+
+export default TextField;
