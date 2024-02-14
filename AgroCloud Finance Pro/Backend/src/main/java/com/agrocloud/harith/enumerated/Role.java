2994ae031907b0ce46edf3fa9a863ad7f035cb6d
Harith Kavish S <94520772+HarithKavish@users.noreply.github.com>
2024-02-14 10:09:12 +0530

    Delete AgroCloud Finance Pro/Backend directory

--- AgroCloud Finance Pro/Backend/src/main/java/com/agrocloud/harith/enumerated/Role.java
+++
@@ -1,5 +0,0 @@
-package com.agrocloud.harith.enumerated;
-
-public enum Role {
-    ADMIN, USER
-}
--- AgroCloud Finance Pro/Backend/src/main/java/com/agrocloud/harith/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agrocloud.harith.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.agrocloud.harith.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, String> {
-
-}
